93d7f23e58c0933ef25fd7bb6732a2801c2022c8
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-11 13:12:41 +0200

    skapar interface course

---
+++ src/app/services/courses-service/courses-service.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-service',
+  imports: [],
+  templateUrl: './courses-service.html',
+  styleUrl: './courses-service.scss',
+})
+export class CoursesService {}
