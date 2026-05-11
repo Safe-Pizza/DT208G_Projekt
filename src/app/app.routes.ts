b0703bf3c76c5cfe015a64c335a99ef0b25c8b79
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-11 12:06:45 +0200

    rensar demo, lägge till komponeneter och grund SASS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Courses } from './pages/courses/courses';
+import { Courseschema } from './pages/courseschema/courseschema';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "courses", component: Courses },
+    { path: "courseschema", component: Courseschema },
+    { path: "**", component: PageNotFound }
+];
---
+++ src/app/pages/courseschema/courseschema.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courseschema',
+  imports: [],
+  templateUrl: './courseschema.html',
+  styleUrl: './courseschema.scss',
+})
+export class Courseschema {}
