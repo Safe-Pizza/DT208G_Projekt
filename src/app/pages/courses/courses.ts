53fea4258c2d0f0ff08f59518b71aa6d6fcc6e16
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-11 15:29:32 +0200

    skriver funktion för filtrering av kurser

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CoursesService } from '../../services/courses-service';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,4 +15,20 @@ export class Courses {
 
   //hämtar kurserna från service
   courses = this.coursesService.getCourses();
+
+  //sökinput
+  searchInput = signal("");
+
+  //filtrering från söktext
+  coursesFiltered = computed(() => {
+    const search = this.searchInput().trim().toLowerCase();
+    if (!search) {
+      return this.courses();
+    }
+
+    return this.courses().filter(c =>
+      c.courseName.toLowerCase().includes(search) ||
+      c.courseCode.toLowerCase().includes(search)
+    );
+  })
 }
