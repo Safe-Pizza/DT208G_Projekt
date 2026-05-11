1f598644bc8ca37b9face32d627998bce1c99246
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-11 13:19:31 +0200

    skriver service för courses-service

--- src/app/services/courses-service/courses-service.ts
+++ src/app/services/courses-service/courses-service.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Signal } from '@angular/core';
+import { Course } from '../../interfaces/course';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-courses-service',
@@ -6,4 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './courses-service.html',
   styleUrl: './courses-service.scss',
 })
-export class CoursesService {}
+export class CoursesService {
+
+  private apiUrl = 'https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json';
+
+  http = inject(HttpClient);
+
+  getCourses(): Signal<Course[]> {
+    const courses$ = this.http.get<Course[]>(this.apiUrl);
+    return toSignal(courses$, { initialValue: [] });
+  };
+}
