b2a5dbb904f5a7e405f19e01e343fac07bca4bd0
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-11 16:09:27 +0200

    lägger till navigerig

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./partials/header/header";
 import { Footer } from "./partials/footer/footer";
+import { MainNav } from "./partials/main-nav/main-nav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, MainNav],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
