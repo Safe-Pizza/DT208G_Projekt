5b1452351b8b935fb1c7a0348ad7532ac08247c2
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-12 13:20:05 +0200

    skriver ut antal kurser, totalt och vid sökning

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -1,7 +1,7 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { CoursesService } from '../../services/courses-service';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Course } from '../../interfaces/course';
 import { CourseschemaService } from '../../services/courseschema-service';
 
@@ -20,20 +20,28 @@ export class Courses {
   //hämtar kurserna från service
   courses = this.coursesService.getCourses();
 
+  //variabler för antal kurser
+  allCourses: number = 0; //totalt antal kurser
+  counter: number = 0; //antal visade kurser
+
   //sökinput
   searchInput = signal("");
 
   //filtrering från söktext
   coursesFiltered = computed(() => {
     const search = this.searchInput().trim().toLowerCase();
+    this.allCourses = this.courses().length;
     if (!search) {
+      this.counter = this.courses().length;
       return this.courses();
     }
 
-    return this.courses().filter(c =>
+    const filteredSearch = this.courses().filter(c =>
       c.courseName.toLowerCase().includes(search) ||
       c.courseCode.toLowerCase().includes(search)
     );
+        this.counter = filteredSearch.length;
+        return filteredSearch;
   })
 
   optionInput = signal("");
@@ -48,8 +56,6 @@ export class Courses {
       c.subjectCode.toLowerCase().includes(option)
     );
   })
-
-  counter = 0;
 
   /*coursesCategorized(): Course[] {
     const subject = this.optionInput.trim().toLocaleLowerCase();
